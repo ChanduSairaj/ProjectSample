207334a5b77e2c54eaeb2a2142a585d741223e42
ChAnDu SaI RaJ <ChAnDu SaI RaJ@Chandu>
2020-04-17 11:55:56 +0530

    commit all files

---
+++ GitHub/src/githubpackage/Sample1.java
@@ -0,0 +1,16 @@
+package githubpackage;
+
+public class Sample1 
+{
+public static void main(String[] args) 
+{
+String s1="chandu";
+System.out.println(s1);
+String s2=s1;
+System.out.println(s2);
+String s3="Sai";
+System.out.println(s3);
+s2=s3;
+System.out.println(s2);
+}
+}
